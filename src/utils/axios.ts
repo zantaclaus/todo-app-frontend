80a693fdc79b47e8285cb6c54ed2c45af0eb72be
zantaclaus <tarzuto@gmail.com>
2023-04-13 16:21:23 +0700

    🛠 Change base url

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,5 +1,5 @@
 import { default as axiosInstance } from 'axios';
 
-const axios = axiosInstance.create({ baseURL: 'http://192.168.64.20:8000' });
+const axios = axiosInstance.create({ baseURL: 'http://192.168.64.21:8000' });
 
 export default axios;
